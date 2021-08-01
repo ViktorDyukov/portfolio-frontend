32cdc2c52eb654e52b371485c610b1571eef6013
Viktor Dyukov <spam@ducov.ru>
2021-08-01 18:31:51 +0300

    webp

--- src/components/_shared/imgSet/imgSet.jsx
+++ src/components/_shared/imgSet/imgSet.jsx
@@ -3,25 +3,21 @@ import { APIUrl } from "../utilities/api";
 
 const ImgSet = ({ imgX1, imgX2 }) => {
   return (
-    // <picture>
-    //   <source
-    //     srcSet={`${APIUrl}${imgX1}.webp 1x, ${APIUrl}${imgX2}.webp 2x,`}
-    //     type="image/webp"
-    //   />
-    //   <source
-    //     srcSet={`${APIUrl}${imgX1} 1x, ${APIUrl}${imgX2}ebp 2x,`}
-    //     type="image/png"
-    //   />
-    //   <img
-    //     srcSet={`${APIUrl}${imgX1} 1x, ${APIUrl}${imgX2} 2x,`}
-    //     src={`${APIUrl}${imgX1}`}
-    //   />
-    // </picture>
-
-    <img
-      srcSet={`${APIUrl}${imgX1} 1x, ${APIUrl}${imgX2} 2x,`}
-      src={`${APIUrl}${imgX1}`}
-    />
+    <picture>
+      <source
+        srcSet={`${APIUrl}${imgX1}.webp 1x, ${APIUrl}${imgX2}.webp 2x,`}
+        type="image/webp"
+      />
+      {/* <source
+        srcSet={`${APIUrl}${imgX1} 1x, ${APIUrl}${imgX2}ebp 2x,`}
+        type="image/png"
+      /> */}
+      {/* <img
+        srcSet={`${APIUrl}${imgX1} 1x, ${APIUrl}${imgX2} 2x,`}
+        src={`${APIUrl}${imgX1}`}
+      /> */}
+      <img />
+    </picture>
   );
 };
 
