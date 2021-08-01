93ed83be0147113c76d5d3f8dd53f2f137a1b944
Viktor Dyukov <spam@ducov.ru>
2021-08-01 14:09:51 +0300

    озуп

--- src/components/_shared/imgSet/imgSet.jsx
+++ src/components/_shared/imgSet/imgSet.jsx
@@ -4,10 +4,10 @@ import { APIUrl } from "../utilities/api";
 const ImgSet = ({ imgX1, imgX2 }) => {
   return (
     <picture>
-      <source
+      {/* <source
         srcSet={`${APIUrl}${imgX1}.webp 1x, ${APIUrl}${imgX2}.webp 2x,`}
         type="image/webp"
-      />
+      /> */}
       <source
         srcSet={`${APIUrl}${imgX1} 1x, ${APIUrl}${imgX2}ebp 2x,`}
         type="image/png"
