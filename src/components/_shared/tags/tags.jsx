90f871f3e5033981ea21a979b8d9d78548f7fef9
Viktor Dyukov <spam@ducov.ru>
2021-08-03 00:15:53 +0300

    mobile - case page

--- src/components/_shared/tags/tags.jsx
+++ src/components/_shared/tags/tags.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import s from "./tags.module.css";
 
 const Tags = (props) => {
-  let op_class = "";
-  if (props.small) {
-    op_class = s.small;
-  }
   let tags = props.list.map((item) => <div>{item.name}</div>);
-  return <div className={`${s.root} ${op_class}`}>{tags}</div>;
+  return <div className={`${s.root}`}>{tags}</div>;
 };
 
 export default Tags;
