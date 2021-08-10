7509c51449095254c81844da03bf13de9fd10af9
Viktor Dyukov <spam@ducov.ru>
2021-08-10 07:13:21 +0300

    fix

--- src/components/homePage/highlight.jsx
+++ src/components/homePage/highlight.jsx
@@ -37,7 +37,7 @@ const Highlight = React.forwardRef((props, ref) => {
     }).from(
       svg_desktop.current,
       {
-        opacity: "0",
+        opacity: "-1",
         ease: "expo.easeOut",
       },
       "<"
