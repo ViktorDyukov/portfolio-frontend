c571f94f6d335f80317021c4af6f4fd241430e92
Viktor Dyukov <spam@ducov.ru>
2021-09-04 01:54:10 +0300

    redirect v1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import "./global.css";
 import MainMenu from "./components/_shared/mainMenu/mainMenu";
 import Footer from "./components/_shared/footer/footer";
 import { BreakpointsProvider } from "react-with-breakpoints";
 import routerData from "./components/_shared/utilities/getRouterData";
 import NotFound from "./components/notFound/notFound";
+import Cookies from "js-cookie";
 import GA4React from "ga-4-react";
 const ga4react = new GA4React("G-G57HD6TQE7");
 ga4react.initialize();
@@ -19,6 +25,24 @@ const breakpoints = {
 };
 
 const App = () => {
+  const hostarr = window.location.host.split(".");
+  const subdomain = hostarr.length == 3 ? hostarr[0] : false;
+  const cookiedomain =
+    hostarr.length == 3
+      ? `.${hostarr[1]}.${hostarr[2]}`
+      : `.${window.location.host}`;
+
+  if (["public", "www", "portfolio", false].includes(subdomain)) {
+    if (Cookies.get("instance")) {
+      if (typeof window !== "undefined") {
+        window.location.href =
+          "//" + Cookies.get("instance") + cookiedomain + "/";
+      }
+    }
+  } else {
+    Cookies.set("instance", subdomain, { domain: ".localvictor.com" });
+  }
+
   return (
     <Router>
       <Route
